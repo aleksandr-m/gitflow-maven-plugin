0c7b64e0f391e1cf33b5e8643cad31d19fc0f81d
Aleksandr Mashchenko <aleksandr.7m@gmail.com>
2015-10-25 15:33:39 +0200

    Quick release mojo

---
+++ src/main/java/com/amashchenko/maven/plugin/gitflow/GitFlowReleaseMojo.java
@@ -0,0 +1,174 @@
+/*
+ * Copyright 2014-2015 Aleksandr Mashchenko.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.amashchenko.maven.plugin.gitflow;
+
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.ArtifactUtils;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.shared.release.versions.DefaultVersionInfo;
+import org.apache.maven.shared.release.versions.VersionParseException;
+import org.codehaus.plexus.components.interactivity.PrompterException;
+import org.codehaus.plexus.util.StringUtils;
+import org.codehaus.plexus.util.cli.CommandLineException;
+
+/**
+ * The git flow release mojo.
+ * 
+ * @author Aleksandr Mashchenko
+ * 
+ */
+@Mojo(name = "release", aggregator = true)
+public class GitFlowReleaseMojo extends AbstractGitFlowMojo {
+
+    /** Whether to skip tagging the release in Git. */
+    @Parameter(property = "skipTag", defaultValue = "false")
+    private boolean skipTag = false;
+
+    /** Whether to skip calling Maven test goal before releasing. */
+    @Parameter(property = "skipTestProject", defaultValue = "false")
+    private boolean skipTestProject = false;
+
+    /** {@inheritDoc} */
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        try {
+            // set git flow configuration
+            initGitFlowConfig();
+
+            // check uncommitted changes
+            checkUncommittedChanges();
+
+            // git for-each-ref --count=1 refs/heads/release/*
+            final String releaseBranch = gitFindBranches(
+                    gitFlowConfig.getReleaseBranchPrefix(), true);
+
+            if (StringUtils.isNotBlank(releaseBranch)) {
+                throw new MojoFailureException(
+                        "Release branch already exists. Cannot start release.");
+            }
+
+            // need to be in develop to get correct project version
+            // git checkout develop
+            gitCheckout(gitFlowConfig.getDevelopmentBranch());
+
+            if (!skipTestProject) {
+                // mvn clean test
+                mvnCleanTest();
+            }
+
+            // get current project version from pom
+            final String currentVersion = getCurrentProjectVersion();
+
+            String defaultVersion = null;
+            if (tychoBuild) {
+                defaultVersion = currentVersion;
+            } else {
+                // get default release version
+                try {
+                    final DefaultVersionInfo versionInfo = new DefaultVersionInfo(
+                            currentVersion);
+                    defaultVersion = versionInfo.getReleaseVersionString();
+                } catch (VersionParseException e) {
+                    if (getLog().isDebugEnabled()) {
+                        getLog().debug(e);
+                    }
+                }
+            }
+
+            if (defaultVersion == null) {
+                throw new MojoFailureException(
+                        "Cannot get default project version.");
+            }
+
+            String version = null;
+            if (settings.isInteractiveMode()) {
+                try {
+                    version = prompter.prompt("What is release version? ["
+                            + defaultVersion + "]");
+                } catch (PrompterException e) {
+                    getLog().error(e);
+                }
+            }
+
+            if (StringUtils.isBlank(version)) {
+                version = defaultVersion;
+            }
+
+            // execute if version changed
+            if (!version.equals(currentVersion)) {
+                // mvn set version
+                mvnSetVersions(version);
+
+                // git commit -a -m updating versions for release
+                gitCommit("updating versions for release");
+            }
+
+            // git checkout master
+            gitCheckout(gitFlowConfig.getProductionBranch());
+
+            // git merge --no-ff develop/...
+            gitMergeNoff(gitFlowConfig.getDevelopmentBranch());
+
+            if (!skipTag) {
+                if (tychoBuild && ArtifactUtils.isSnapshot(version)) {
+                    version = version.replace("-" + Artifact.SNAPSHOT_VERSION,
+                            "");
+                }
+
+                // git tag -a ...
+                gitTag(gitFlowConfig.getVersionTagPrefix() + version,
+                        "tagging release");
+            }
+
+            // git checkout develop
+            gitCheckout(gitFlowConfig.getDevelopmentBranch());
+
+            String nextSnapshotVersion = null;
+            // get next snapshot version
+            try {
+                final DefaultVersionInfo versionInfo = new DefaultVersionInfo(
+                        version);
+                nextSnapshotVersion = versionInfo.getNextVersion()
+                        .getSnapshotVersionString();
+            } catch (VersionParseException e) {
+                if (getLog().isDebugEnabled()) {
+                    getLog().debug(e);
+                }
+            }
+
+            if (StringUtils.isBlank(nextSnapshotVersion)) {
+                throw new MojoFailureException(
+                        "Next snapshot version is blank.");
+            }
+
+            // mvn set version
+            mvnSetVersions(nextSnapshotVersion);
+
+            // git commit -a -m updating for next development version
+            gitCommit("updating for next development version");
+
+            if (installProject) {
+                // mvn clean install
+                mvnCleanInstall();
+            }
+        } catch (CommandLineException e) {
+            getLog().error(e);
+        }
+    }
+}
