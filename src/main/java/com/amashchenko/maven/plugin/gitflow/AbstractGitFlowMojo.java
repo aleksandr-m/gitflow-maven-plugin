dfd91aa7f40a291fa32bc348051cba6e17f02df5
Arik Kfir <arik@personal.com>
2017-06-02 15:08:19 +0300

    incorrectly specified all command-line arguments to versions:set in a single string, instead of splitting them into multiple strings

--- src/main/java/com/amashchenko/maven/plugin/gitflow/AbstractGitFlowMojo.java
+++ src/main/java/com/amashchenko/maven/plugin/gitflow/AbstractGitFlowMojo.java
@@ -714,8 +714,7 @@ public abstract class AbstractGitFlowMojo extends AbstractMojo {
             executeMvnCommand(TYCHO_VERSIONS_PLUGIN_SET_GOAL, "-DnewVersion="
                     + version, "-Dtycho.mode=maven");
         } else {
-            executeMvnCommand(VERSIONS_MAVEN_PLUGIN_SET_GOAL, "-B -DgroupId=\"*\" -DartifactId=\"*\" -DnewVersion="
-                    + version, "-DgenerateBackupPoms=false");
+            executeMvnCommand(VERSIONS_MAVEN_PLUGIN_SET_GOAL, "-B", "-DgroupId=\"*\"", "-DartifactId=\"*\"", "-DnewVersion=" + version, "-DgenerateBackupPoms=false");
         }
     }
 
