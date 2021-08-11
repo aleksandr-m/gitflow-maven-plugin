e2579cf456829a7208c147606d4be2c05404357f
Kutsal Berberoglu <kutsal.berberoglu@dominos.com>
2021-08-11 15:09:04 -0400

    Should've been an abstract class

--- src/main/java/com/amashchenko/maven/plugin/gitflow/CommandLineSettable.java
+++ src/main/java/com/amashchenko/maven/plugin/gitflow/CommandLineSettable.java
@@ -6,9 +6,18 @@ import java.beans.PropertyDescriptor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Map;
 import java.util.Map.Entry;
+import org.codehaus.plexus.configuration.PlexusConfiguration;
 
-public class CommandLineSettable {
+public abstract class CommandLineSettable {
 
+    /**
+     * Called by {@link org.eclipse.sisu.plexus.CompositeBeanHelper#setDefault(Object, Object, PlexusConfiguration)} during initialization
+     *
+     * @param cliConfig
+     * @throws IntrospectionException
+     * @throws InvocationTargetException
+     * @throws IllegalAccessException
+     */
     public void set(String cliConfig) throws IntrospectionException, InvocationTargetException, IllegalAccessException {
         Map<String, String> values = Splitter.on(",").withKeyValueSeparator("=").split(cliConfig);
         for (Entry<String, String> e : values.entrySet()) {
