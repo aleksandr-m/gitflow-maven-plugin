fa72893c96ac6cfe6942bf1677a856e17494fc58
Dennis Roppelt <ddroppelt@gmail.com>
2023-05-16 20:05:44 +0200

    latest maven-versions plugin

--- src/main/java/com/amashchenko/maven/plugin/gitflow/AbstractGitFlowMojo.java
+++ src/main/java/com/amashchenko/maven/plugin/gitflow/AbstractGitFlowMojo.java
@@ -209,8 +209,8 @@ public abstract class AbstractGitFlowMojo extends AbstractMojo {
      * 
      * @since 1.18.0
      */
-    @Parameter(property = "versionsMavenPluginVersion", defaultValue = "2.14.2")
-    private String versionsMavenPluginVersion = "2.24.2";
+    @Parameter(property = "versionsMavenPluginVersion", defaultValue = "2.15.0")
+    private String versionsMavenPluginVersion = "2.25.0";
 
     /**
      * Version of tycho-versions-plugin to use.
