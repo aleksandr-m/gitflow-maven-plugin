444ac422851c32b8972c6fcaf8f16634842c1cae
Aleksandr Mashchenko <aleksandr.7m@gmail.com>
2024-02-10 15:19:45 +0200

    Update copyright notice

--- src/test/java/com/amashchenko/maven/plugin/gitflow/NextHotfixVersionTest.java
+++ src/test/java/com/amashchenko/maven/plugin/gitflow/NextHotfixVersionTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2023 Aleksandr Mashchenko.
+ * Copyright 2014-2024 Aleksandr Mashchenko.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
